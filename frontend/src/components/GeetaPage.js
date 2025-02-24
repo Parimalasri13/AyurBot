3d1e6439802ff17f3c23e6e5285aa8d06da3dd80
Parimalasri13 <115871827+Parimalasri13@users.noreply.github.com>
2025-02-24 11:47:34 +0530

    Update GeetaPage.js

--- frontend/src/components/GeetaPage.js
+++ frontend/src/components/GeetaPage.js
@@ -43,7 +43,7 @@ const GeetaPage = () => {
         {/* Krishna Image */}
         <div className="image-container">
           <img
-            src="./assets/krish.jpg"
+            src="./assets/image1.png"
             alt="Lord Krishna"
             className="krishna-image"
           />
